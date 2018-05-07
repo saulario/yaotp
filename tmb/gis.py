d4631a31d8669d811c25d3f51a4c635e643e4682
ctl <saulario@gmail.com>
2018-05-06 22:48:04 -0400

    Eliminando logger en código ya totalmente estático

--- tmb/gis.py
+++ tmb/gis.py
@@ -14,24 +14,15 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import logging
 import re
-
-log = logging.getLogger(__name__)
 
 coordenada_pattern = re.compile("^(?P<grados>\d+):(?P<minutos>\d+):(?P<segundos>\d+\.\d+)(?P<zona>[NSEW])$")
 
-#
-#
-#
 def text2float(coordenada):
-    log.info("-----> Inicio")
-    log.info("\t(coordenada): %s" % coordenada)
     
     retval = None
     match = coordenada_pattern.match(coordenada)
     if (not match):
-        log.info("<----- Fin, coordenada no valida")
         return retval;
 
     grados = float(match.group("grados"))
@@ -43,34 +34,22 @@ def text2float(coordenada):
     if zona in "SW":
         retval *= (-1)
 
-    log.info("<----- Fin")
     return retval
 
-#
-#
-#
 def convertir_coordenada_GPS(latitud, longitud):
-    log.info("-----> Inicio")
-    log.info("\t(latitud): %s" % latitud)
-    log.info("\t(longitud): %s" % longitud)
     
     retval = None
     
     lat = text2float(latitud)
     if lat is None:
-        log.info("<----- Fin, latitud invalida")
         return retval
     
     lon = text2float(longitud)
     if lon is None:
-        log.info("<----- Fin, longitud invalida")
         return retval
 
     retval = {}
     retval["type"] = "Point"
     retval["coordinates"] = [ lat, lon ]
     
-    log.info("<----- Fin")
-    return retval
-    
-    
+    return retval    
