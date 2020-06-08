3f23e0ac6ec1ee3b44e41378e8fc58bfddaba1f5
saulario <saul.correas@gmail.com>
2020-06-08 20:23:24 +0200

    Salvaguarda

--- dbm/environment.py
+++ dbm/environment.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 import os
+import pymongo
+import sqlalchemy
+
+from context import Context
 
 DBMANAGER_HOME = ("%s/tdi/dbmanager-1.0" % os.path.expanduser("~"))
 LOG_FORMAT = "%(asctime)s %(levelname)s %(module)s.%(funcName)s %(message)s"
@@ -63,6 +67,7 @@ def borrar_watchdog(cola, nombre):
     if os.path.exists(watchdog):
         os.remove(watchdog)
 
+
 def obtener_nombre_instancia(cp):
     """
     El nombre de la instancia se obtiene del fichero de
@@ -72,12 +77,14 @@ def obtener_nombre_instancia(cp):
     """
     return cp.get("TDI", "cola")
 
+
 def obtener_nombre_archivo(fichero):
     """
     Para evitar escribir siempre esta instrucción
     :param fichero: el valor de __file__ del fichero que se quiere obtener
     """
     return os.path.basename(fichero).split(".")[0]
+
 
 def obtener_nombre_archivo_log(instancia, archivo):
     """
@@ -87,3 +94,29 @@ def obtener_nombre_archivo_log(instancia, archivo):
     """
     return ("%s/%s/log/%s.log" % (DBMANAGER_HOME,
             instancia, archivo))
+
+def obtener_contexto_desde_configuracion(cp):
+    """
+    Genera un objeto Context desde los parámetros de configuración
+    :param cp: parámetros de configuración
+    """
+    context = Context()
+
+    context.home = DBMANAGER_HOME
+    context.url = ("%s/tdi/AMMForm?" % cp.get("TDI", "url_formatos"))
+    context.queue = cp.get("TDI", "cola")
+    context.user = cp.get("TDI", "user")
+    context.password = cp.get("TDI", "password")
+    context.batch_size = cp.get("TDI", "batch_size")
+
+    context.client = pymongo.MongoClient(cp.get("MONGO", "uri"))
+    context.db = context.client.get_database(cp.get("MONGO", "db"))
+    context.debug = cp.getint("MONGO", "debug")
+
+    context.sql_engine = sqlalchemy.create_engine(cp.get("SQL", "uri"),
+            pool_pre_ping = True, pool_recycle = int(cp.get("SQL", "recycle")))
+    context.sql_metadata = sqlalchemy.MetaData(bind = context.sql_engine)
+
+    return context
+
+
--- dbm/reader.py
+++ dbm/reader.py
@@ -7,14 +7,73 @@ import os
 import os.path
 import pymongo
 import sys
+import threading
+import time
 
 from context import Context
 from environment import *
 
 log = logging.getLogger(__name__)
 
-def main(cp):
-    pass
+class LectorColas(threading.Thread):
+    """
+    Lee las colas de T-Mobility e inserta directamente en colas AMQP
+    """
+    def __init__(self, worker):
+        super().__init__()
+        self.worker = worker
+
+    def run(self):
+        print("LectorColas")
+
+class ProcesarComandos(threading.Thread):
+    """
+    Escucha la cola de comandos 
+    """
+    def __init__(self, worker):
+        super().__init__()
+        self.worker = worker
+
+    def run(self):
+        print("ProcesarComandos")
+
+class EnviarEstado(threading.Thread):
+    """
+    Envía estadísticas de ejecución
+    """
+    def __init__(self, worker):
+        super().__init__()
+        self.worker = worker
+
+    def run(self):
+        print("EnviarEstados")
+
+class Worker():
+    """
+    Clase para coordinar la comunicación entre threads
+    """
+    def __init__(self, context):
+        self.context = context
+
+
+    def run(self):
+        """
+        Lanza los threads
+        """
+        threads = []
+
+        thread = LectorColas(self)
+        threads.append(thread)
+
+        thread = ProcesarComandos(self)
+        threads.append(thread)
+
+        thread = EnviarEstado(self)
+        threads.append(thread)
+
+        for thread in threads:
+            thread.start()
+            thread.join()
 
 
 if __name__ == "__main__":
@@ -37,6 +96,8 @@ if __name__ == "__main__":
         sys.stderr.write(e)
         sys.exit(1)
 
+    context = obtener_contexto_desde_configuracion(cp)
+
     CURRENT_INSTANCE = obtener_nombre_instancia(cp)
     CURRENT_FILE = obtener_nombre_archivo(__file__)
     LOG_FILE = obtener_nombre_archivo_log(CURRENT_INSTANCE,
@@ -49,7 +110,7 @@ if __name__ == "__main__":
     try:
         if not existe_instancia_activa(CURRENT_INSTANCE,
                     CURRENT_FILE, os.getpid()):
-            main(cp)
+            Worker(context).run()
             borrar_watchdog(CURRENT_INSTANCE, CURRENT_FILE)
         else:
             log.warn("*** Saliendo, existe una instancia en ejecución")
